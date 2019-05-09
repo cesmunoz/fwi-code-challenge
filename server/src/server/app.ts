5c1b40419b04a83e0909463a90772a1a5d9de9a5
Cesar Muñoz <cesar.alberto.munoz@gmail.com>
2019-05-09 18:38:54 -0300

    Allow running parallel projects

--- server/src/server/app.ts
+++ server/src/server/app.ts
@@ -1,9 +1,12 @@
 import * as express from "express";
+import { Request, Response } from "express";
 import * as bodyParser from "body-parser";
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req: Request, res: Response) => res.status(200).send('Welcome to FWI API'));
 
 export default app;
